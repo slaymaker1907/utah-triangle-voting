73431bd1ede50313765b8a2bef30344f11448d77
slaymaker1907 <dyllongagnier@gmail.com>
2015-12-13 05:44:18 -0700

    Added a vote page

--- triangle_website/voting/urls.py
+++ triangle_website/voting/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 app_name = 'voting'
 urlpatterns = [
-	url(r'^$', voting_index, name='index')
+	url(r'^$', voting_index, name='index'),
+	url(r'^vote/(\d+)$', vote_page, name='vote')
 ]
--- triangle_website/voting/views.py
+++ triangle_website/voting/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def voting_index(request):
 	return render(request, 'voting/index.html')
+	
+def vote_page(request, vote_id):
+	return render(request, 'voting/vote.html')
