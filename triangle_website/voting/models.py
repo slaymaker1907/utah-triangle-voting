8a1739dce9e5a4c25a4fe72a4e9d33c9278a9634
slaymaker1907 <dyllongagnier@gmail.com>
2015-12-14 00:43:39 -0700

    Made the alt voting dynamic

--- triangle_website/voting/models.py
+++ triangle_website/voting/models.py
@@ -3,7 +3,24 @@ from django.db import models
 # Not currently a full model with access to DB.
 class Vote:
 	__id__ = 0
-	def __init__(self, name=""):
+	def __init__(self, name="", is_poll=False):
 		self.name = name
 		self.id = Vote.__id__ + 1
 		Vote.__id__ += 1
+		self.questions = []
+		self.is_poll = is_poll
+		
+class Question:
+	__id__ = 0
+	def __init__(self, name=""):
+		self.name = name
+		self.choices = []
+		self.id = Question.__id__
+		Question.__id__ += 1
+		
+class Choice:
+	__id__ = 0
+	def __init__(self, text):
+		self.text = text
+		self.id = Choice.__id__
+		Choice.__id__ += 1
--- triangle_website/voting/views.py
+++ triangle_website/voting/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 
-old_votes = [Vote("Best Santa Clause"), Vote("World Peace"), Vote("Best LoL Player"), Vote("Religous Freedom"), Vote("Strong Encryption"), Vote("Favorite Color")]
+old_votes = [Vote("Best Politicians"), Vote("World Peace"), Vote("Best LoL Player"), Vote("Religous Freedom"), Vote("Strong Encryption"), Vote("Favorite Color")]
 current_votes = [Vote("President Fall 2016"), Vote("Sandwich Maker 2016"), Vote("Legislation 01/25/16")]
+
+old_votes[0].questions = [Question("National"), Question("Local")]
+old_votes[0].questions[0].choices = [Choice("Barrack Obama"), Choice("Donald Trump"), Choice("Hilary Clinton")]
+old_votes[0].questions[1].choices = [Choice("John Huntsman Jr."), Choice("Jim Matheson"), Choice("Gary Herbert"), Choice("Bob Bennet")]
+
+current_votes[0].questions = [Question("President"), Question("Political Party")]
+current_votes[0].questions[0].choices = [Choice("Bernie Sanders"), Choice("Donald Trump"), Choice("Jeb Bush")]
+current_votes[0].questions[1].choices = [Choice("Democrat"), Choice("Republican"), Choice("Libertarian"), Choice("Independent")]
+
+def get_first(iter, pred):
+	for ele in iter:
+		if pred(ele):
+			return ele
+	return None
 
 # Create your views here.
 def voting_index(request):
@@ -11,12 +25,18 @@ def voting_index(request):
 	return render(request, 'voting/index.html', context)
 	
 def vote_page(request, vote_id):
-	if vote_id == "1234":
-		return render(request, 'voting/vote.html')
+	id = int(vote_id)
+	vote = get_first(current_votes, lambda vt: vt.id == id)
+	if vote.is_poll:
+		temp = 'voting/vote_poll.html'
 	else:
-		return render(request, 'voting/vote_poll.html')
-	
+		temp = 'voting/vote.html'
+	return render(request, temp, {"vote":vote})
+				
 def results_page(request, vote_id):
+	if request.method == "POST":
+		for param in request.POST:
+			print(param + ":" + request.POST[param])
 	if vote_id == "0":
 		return render(request, 'voting/results.html')
 	else:
