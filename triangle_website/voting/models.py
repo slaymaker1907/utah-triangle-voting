0c5d0f014a28c8d8c0037d26301699db79287361
slaymaker1907 <dyllongagnier@gmail.com>
2015-12-13 22:18:26 -0700

    Made index actually use a model

--- triangle_website/voting/models.py
+++ triangle_website/voting/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+# Not currently a full model with access to DB.
+class Vote:
+	__id__ = 0
+	def __init__(self, name=""):
+		self.name = name
+		self.id = Vote.__id__ + 1
+		Vote.__id__ += 1
--- triangle_website/voting/views.py
+++ triangle_website/voting/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
+
+old_votes = [Vote("Best Santa Clause"), Vote("World Peace"), Vote("Best LoL Player"), Vote("Religous Freedom"), Vote("Strong Encryption"), Vote("Favorite Color")]
+current_votes = [Vote("President Fall 2016"), Vote("Sandwich Maker 2016"), Vote("Legislation 01/25/16")]
 
 # Create your views here.
 def voting_index(request):
-	return render(request, 'voting/index.html')
+	context = {"current_votes": current_votes, "complete_votes":old_votes[:5]}
+	return render(request, 'voting/index.html', context)
 	
 def vote_page(request, vote_id):
 	if vote_id == "1234":
