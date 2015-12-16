307fc1540486abd6498566d496e1a7fda7d326f7
Dyllon Gagnier <dyllongagnier@gmail.com>
2015-12-16 12:01:49 -0700

    Got creating a vote to mostly work except for passcode and auth

--- triangle_website/voting/views.py
+++ triangle_website/voting/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from django.core.urlresolvers import reverse
+import itertools
 
 # Create your views here.
 def voting_index(request):
@@ -24,7 +25,7 @@ def results_page(request, vote_id):
 		return render(request, 'voting/results.html')
 	else:
 		return render(request, 'voting/results_poll.html')
-	
+
 def new_vote(request):
 	return render(request, 'voting/new_vote.html')
 	
@@ -32,7 +33,29 @@ def history(request, page):
 	return render(request, 'voting/history.html')
 	
 def create_vote(request):
+	new_vote = Election()
 	if request.method == "POST":
 		for param in request.POST:
 			print(param + ":" + request.POST[param])
+	new_vote.is_poll = request.POST["voteType"] == "poll"
+	new_vote.name = request.POST["voteName"]
+	new_vote.save()
+	for quesCount in itertools.count(1):
+		quesId = 'q' + str(quesCount)
+		if quesId in request.POST:
+			newQues = Question()
+			newQues.name = request.POST[quesId]
+			newQues.election = new_vote
+			newQues.save()
+			for choiceCount in itertools.count(1):
+				choiceId = quesId + 'c' + str(choiceCount)
+				if choiceId in request.POST:
+					newChoice = Choice()
+					newChoice.name = request.POST[choiceId]
+					newChoice.question = newQues
+					newChoice.save()
+				else:
+					break
+		else:
+			break
 	return HttpResponseRedirect(reverse('voting:index'))
