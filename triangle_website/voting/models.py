86d8ef54f5c2ba7c93761bc2d8384273691fc285
slaymaker1907 <dyllongagnier@gmail.com>
2015-12-17 21:10:10 -0700

    Added code to check for a passcode if requested

--- triangle_website/voting/models.py
+++ triangle_website/voting/models.py
@@ -9,8 +9,24 @@ class Election(models.Model):
 	name = models.CharField(max_length=255)
 	is_poll = models.BooleanField()
 	is_open = models.BooleanField(default=True)
+	
 	def can_vote(self, user_ob):
 		return len(Voter.objects.filter(user=user_ob).filter(election=self)) == 0
+	
+	# Returns either the passcode (which must be unique by db) or None if there is no passcode.
+	def get_passcode(self):
+		return Passcode.objects.filter(election=self).first()
+		
+	# Returns true if the passcode is valid or if there is no passcode.
+	# This accepts a function so that getting a passcode is only invoked if
+	# a passcode is actually needed.
+	def check_passcode(self, get_passcode):
+		code = self.get_passcode()
+		return not (code != None and code.code != get_passcode())
+		
+class Passcode(models.Model):
+	code = models.CharField(max_length=255)
+	election = models.ForeignKey(Election, on_delete=models.CASCADE, unique=True)
 		
 class Voter(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -61,3 +77,7 @@ class Vote(models.Model):
 	rank = models.IntegerField()
 	# This is anonymous and can not be traced back to a user.
 	voter = models.ForeignKey(AnonVoter, on_delete=models.CASCADE)
+	
+class InvPasscode(Exception):
+	def __init__(self, message=''):
+		self.message = message
--- triangle_website/voting/views.py
+++ triangle_website/voting/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 from django.core.urlresolvers import reverse
 import itertools
+from django.db import transaction
 
 # Create your views here.
 def voting_index(request):
@@ -19,36 +20,34 @@ def vote_page(request, vote_id):
 		temp = 'voting/vote.html'
 	return render(request, temp, {"vote":vote})
 	
+@transaction.atomic
 def submit_vote(request, vote_id):
 	# Should never fail except for very strange circumstances
-	modified = []
-	try:
-		vote = Election.objects.get(pk=vote_id)
-		voter = AnonVoter(election=vote)
-		voter.save()
-		modified.append(voter)
-		# TODO Make sure users can't vote twice
-		if (vote.is_poll):
-			for question in vote.question_set.all():
-				id = str(question.id)
-				if id in request.POST:
-					selected = int(request.POST[str(question.id)])
-					new_vote = Vote(choice=Choice.objects.get(pk=selected), rank=1, voter=voter)
+	vote = Election.objects.get(pk=vote_id)
+	
+	# Check passcode
+	if not vote.check_passcode(lambda: request.POST['passcode']):
+		raise InvPasscode()
+	
+	voter = AnonVoter(election=vote)
+	voter.save()
+	
+	# TODO Make sure users can't vote twice
+	if (vote.is_poll):
+		for question in vote.question_set.all():
+			id = str(question.id)
+			if id in request.POST:
+				selected = int(request.POST[str(question.id)])
+				new_vote = Vote(choice=Choice.objects.get(pk=selected), rank=1, voter=voter)
+				new_vote.save()
+	else: # Vote is alternative vote.
+		for question in vote.question_set.all():
+			for choice in question.choice_set.all():
+				opt = str(question.id) + ':' + str(choice.id)
+				if opt in request.POST:
+					# TODO Verify vote data.
+					new_vote = Vote(rank=int(request.POST[opt]), choice=choice, voter=voter)
 					new_vote.save()
-					modified.append(new_vote)
-		else: # Vote is alternative vote.
-			for question in vote.question_set.all():
-				for choice in question.choice_set.all():
-					opt = str(question.id) + ':' + str(choice.id)
-					if opt in request.POST:
-						# TODO Verify vote data.
-						new_vote = Vote(rank=int(request.POST[opt]), choice=choice, voter=voter)
-						new_vote.save()
-						modified.append(new_vote)
-	except:
-		for data in modified:
-			data.delete()
-		raise
 	return HttpResponseRedirect(reverse('voting:results', args=[str(vote.id)]))
 					
 def results_page(request, vote_id):
@@ -68,32 +67,28 @@ def history(request, page):
 def delete_votes():
 	for elec in Election.objects.all():
 		elec.delete()
-	
+
+@transaction.atomic
 def create_vote(request):
 	new_vote = Election()
 	new_vote.is_poll = request.POST["voteType"] == "poll"
 	new_vote.name = request.POST["voteName"]
 	new_vote.save()
-	try:
-		for quesCount in itertools.count(1):
-			quesId = 'q' + str(quesCount)
-			if quesId in request.POST:
-				newQues = Question()
-				newQues.name = request.POST[quesId]
-				newQues.election = new_vote
-				newQues.save()
-				for choiceCount in itertools.count(1):
-					choiceId = quesId + 'c' + str(choiceCount)
-					if choiceId in request.POST:
-						newChoice = Choice()
-						newChoice.text = request.POST[choiceId]
-						newChoice.question = newQues
-						newChoice.save()
-					else:
-						break
-			else:
-				break
-	except:
-		new_vote.delete()
-		raise
+	if request.POST['useCode']:
+		passcode = Passcode(code=request.POST['passcode'], election=new_vote)
+		passcode.save()
+	for quesCount in itertools.count(1):
+		quesId = 'q' + str(quesCount)
+		if quesId in request.POST:
+			new_ques = Question(name=request.POST[quesId], election=new_vote)
+			new_ques.save()
+			for choiceCount in itertools.count(1):
+				choiceId = quesId + 'c' + str(choiceCount)
+				if choiceId in request.POST:
+					newChoice = Choice(text=request.POST[choiceId], question=new_ques)
+					newChoice.save()
+				else:
+					break
+		else:
+			break
 	return HttpResponseRedirect(reverse('voting:index'))
