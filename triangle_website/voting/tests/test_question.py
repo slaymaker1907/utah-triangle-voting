a317a61b6f686b6f1821d9bc4f8971e4eff7489c
Dyllon Gagnier <dyllongagnier@gmail.com>
2015-12-20 01:57:57 -0700

    Added a test for voting

--- triangle_website/voting/tests/test_question.py
+++ triangle_website/voting/tests/test_question.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from voting.models import Question, Election, Choice
+from voting.models import *
 from voting.tests.helpers import UserBank
 
 class TestGetResults(TestCase):
@@ -11,9 +11,21 @@ class TestGetResults(TestCase):
 		self.choice2 = Choice.objects.create(text='choice2', question=self.ques)
 		self.choice3 = Choice.objects.create(text='choice3', question=self.ques)
 		
+	def assertVoteEqual(self, actual, expected):
+		self.assertEqual(len(actual), len(expected))
+		for actWin, expWin in zip(actual, expected):
+			self.assertSetEqual(actWin, expWin)
+		
 	# If no votes, it should be a tie among all contestants.
 	def test_no_votes(self):
 		result = self.ques.get_results()
-		self.assertEqual(len(result), 1)
-		self.assertSetEqual(result[0], {self.choice1, self.choice2, self.choice3})
+		self.assertVoteEqual(result, [{self.choice1, self.choice2, self.choice3}])
 		
+	def vote(self, elec, ranking):
+		voter = AnonVoter.objects.create(election=elec)
+		for choice, rank in ranking.items():
+			Vote.objects.create(voter=voter, choice=choice, rank=rank)
+		
+	def test_basic_vote(self):
+		self.vote(self.elec, {self.choice1:1, self.choice2:2, self.choice3:3})
+		self.assertVoteEqual(self.ques.get_results(), [{choice} for choice in [self.choice1, self.choice2, self.choice3]])
