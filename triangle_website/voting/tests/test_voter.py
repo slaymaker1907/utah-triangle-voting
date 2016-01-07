a06025b2f29ec110bb4d35e4622ebba3c46448f7
Dyllon Gagnier <dyllongagnier@gmail.com>
2016-01-07 11:38:46 -0700

    Added a test to check that it returns None when no further options

--- triangle_website/voting/tests/test_voter.py
+++ triangle_website/voting/tests/test_voter.py
@@ -1,12 +1,31 @@
 from django.test import TestCase
-from voting.models import Voter, Election
+from voting.models import *
 from voting.tests.helpers import UserBank
 from django.db.utils import IntegrityError
 
 class VoterTest(TestCase):
+	def setUp(self):
+		self.users = UserBank(5)
+		self.elec = Election.objects.create(name='elec', is_poll=False, creator=self.users.get(0))
+		self.ques = Question.objects.create(name='ques', election=self.elec)
+		self.choice1 = Choice.objects.create(text='choice1', question=self.ques)
+		self.choice2 = Choice.objects.create(text='choice2', question=self.ques)
+		self.choice3 = Choice.objects.create(text='choice3', question=self.ques)
+		
+	def vote(self, elec, ranking):
+		voter = AnonVoter.objects.create(election=elec)
+		for choice, rank in ranking.items():
+			Vote.objects.create(voter=voter, choice=choice, rank=rank)
+		return voter
+
 	def test_mutli_user_test(self):
-		user = UserBank(1).get(0)
+		user = self.users.get(0)
 		elec = Election.objects.create(name='elec', is_poll=False, creator=user)
 		Voter.objects.create(election=elec, user=user)
 		with self.assertRaises(IntegrityError):
 			Voter.objects.create(election=elec, user=user)
+			
+	# Tests that if a partial vote, returns None if all choices are excluded that the user selected.
+	def test_partial_highest_rank(self):
+		voter = self.vote(self.elec, {self.choice1:1, self.choice2:2})
+		self.assertIsNone(voter.get_first_choice(self.ques, exclude={self.choice1, self.choice2}))
