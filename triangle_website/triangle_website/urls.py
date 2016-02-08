65610affabd6d3b6bf7f3d4c98ad4cb5983e2394
Dyllon Gagnier <dyllongagnier@gmail.com>
2016-02-08 13:29:46 -0700

    Added password reset

--- triangle_website/triangle_website/urls.py
+++ triangle_website/triangle_website/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	url(r'^voting/', include('voting.urls')),
     url(r'^admin/', admin.site.urls),
-	url(r'^', include('voting.default_urls'))
+	url('^auth/', include('django.contrib.auth.urls')),
+	url(r'^', include('voting.default_urls')),
 ]
